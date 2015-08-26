22228b256272d2fa95f334601619ca237977e542
Jan Marcek <jan.marcek@eea.sk>
2015-08-26 14:34:16 +0200

    backup script - use tar.gz instead zip. tar.gz preserve permissions

--- backup/backup.sh
+++ backup/backup.sh
@@ -18,7 +18,7 @@ mkdir -p $BACKUP_TMP_DIR
 echo "unifiedviews"
 BACKUP_DIR_UV=$BACKUP_TMP_DIR/unifiedviews
 mkdir -p $BACKUP_DIR_UV
-zip -r $BACKUP_DIR_UV/dpus.zip /var/lib/unifiedviews/common
+tar -zcvpf $BACKUP_DIR_UV/dpus.tar.gz /var/lib/unifiedviews/common
 su - postgres -c "pg_dump unifiedviews --inserts" > $BACKUP_DIR_UV/unifiedviews.sql
 # ckan ic, pc,  
 echo "ckan"
@@ -30,47 +30,50 @@ mkdir -p $BACKUP_DIR_CKAN
 su - postgres -c "pg_dump datastore-odn-ic --inserts" > $BACKUP_DIR_CKAN/datastore-odn-ic.sql
 su - postgres -c "pg_dump datastore-odn-pc --inserts" > $BACKUP_DIR_CKAN/datastore-odn-pc.sql
 # filestore-odn-ic, filestore-odn-pc 
-zip -r $BACKUP_DIR_CKAN/filestore-odn-ic.zip /var/lib/odn-ckan-ic/
-zip -r $BACKUP_DIR_CKAN/filestore-odn-pc.zip /var/lib/odn-ckan-pc/
+tar -zcvpf $BACKUP_DIR_CKAN/filestore-odn-ic.tar.gz /var/lib/odn-ckan-ic/
+tar -zcvpf $BACKUP_DIR_CKAN/filestore-odn-pc.tar.gz /var/lib/odn-ckan-pc/
 # dumps
 echo "dumps"
 BACKUP_DIR_DUMP=$BACKUP_TMP_DIR/dump
 mkdir -p $BACKUP_DIR_DUMP
-zip -r $BACKUP_DIR_DUMP/dumps.zip  /var/www/dump
+tar -zcvpf $BACKUP_DIR_DUMP/dumps.tar.gz  /var/www/dump
+
 # ldap
 echo "LDAP"
 BACKUP_DIR_LDAP=$BACKUP_TMP_DIR/ldap
 mkdir -p $BACKUP_DIR_LDAP
-zip -r $BACKUP_DIR_LDAP/ldap.zip /var/lib/ldap_odn
+tar -zcvpf $BACKUP_DIR_LDAP/ldap.tar.gz /var/lib/ldap_odn
+
 # conf
 echo "conf"
 BACKUP_DIR_CONF=$BACKUP_TMP_DIR/conf
 mkdir -p $BACKUP_DIR_CONF
-zip -r  $BACKUP_DIR_CONF/conf.zip /etc/default/tomcat7  /etc/apache2  /etc/odn-cas /etc/odn-midpoint  /etc/odn-simple  /etc/odn-solr  /etc/unfiedviews
+tar -zcvpf $BACKUP_DIR_CONF/conf.tar.gz /etc/default/tomcat7  /etc/apache2  /etc/odn-cas /etc/odn-midpoint  /etc/odn-simple  /etc/odn-solr  /etc/unifiedviews
+
 # idm
 echo "idm"
 BACKUP_DIR_IDM=$BACKUP_TMP_DIR/idm
 mkdir -p $BACKUP_DIR_IDM
-zip -r $BACKUP_DIR_IDM/midpoint.home.zip /var/lib/midpoint.home
+tar -zcvpf $BACKUP_DIR_IDM/midpoint.home.tar.gz /var/lib/midpoint.home
 su - postgres -c "pg_dump midpoint --inserts" > $BACKUP_DIR_IDM/midpoint.sql
 # apt debian packages
 echo "apt debian packages"
 APT_PACKAGE=$BACKUP_TMP_DIR/deb
 mkdir -p $APT_PACKAGE
 dpkg -l >  $APT_PACKAGE/packages.list
-cp -R /etc/apt/sources.list* $APT_PACKAGE/
+cp -pR /etc/apt/sources.list* $APT_PACKAGE/
 apt-key exportall > $APT_PACKAGE/repo.keys
 # virtuoso
 echo "virtuoso"
 BACKUP_DIR_VIRTUOSO=$BACKUP_TMP_DIR/virtuoso
 mkdir -p $BACKUP_DIR_VIRTUOSO
-zip -r $BACKUP_DIR_VIRTUOSO/virtuoso.zip /var/lib/virtuoso-opensource-7
+tar -zcvpf  $BACKUP_DIR_VIRTUOSO/virtuoso.tar.gz /var/lib/virtuoso-opensource-7
 # final backup file
 echo "create backup file: $BACKUP_NAME"
 cd $BACKUP_PATH
-zip -r $BACKUP_PATH/$BACKUP_NAME.zip $BACKUP_NAME
+tar -zcvpf  $BACKUP_PATH/$BACKUP_NAME.tar.gz $BACKUP_NAME
 timestamp=$(date +"%Y-%m-%d_%H:%M:%S")
 echo "clean tmp files"
 rm -rf $BACKUP_TMP_DIR
-echo "backup file: $BACKUP_PATH/$BACKUP_NAME.zip was successfully created on: $timestamp"
+echo "backup file: $BACKUP_PATH/$BACKUP_NAME.tar.gz was successfully created on: $timestamp"
 exit 0
